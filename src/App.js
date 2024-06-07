e6b245c602304dac7dca8385c89abddeba08fd7e
rjm <robjmass@gmail.com>
2024-06-07 12:30:47 -0400

    unnamed commit

--- src/App.js
+++ src/App.js
@@ -3,52 +3,49 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [afc, setAfc] = useState(0);
-    const [tfc, setTfc] = useState(0);
-    const [q, setQ] = useState(0);
+    // const [afc, setAfc] = useState(0);
+    // const [tfc, setTfc] = useState(0);
+    // const [q, setQ] = useState(0);
+    const [inputs, setInputs] = useState([
+        { name: 'afc', value: 0 },
+        { name: 'tfc', value: 0 },
+        { name: 'q', value: 0 }
+    ]);
 
-    const handleAfcChange = (e) => {
-        setAfc(e.target.value);
-    };
-
-    const handleTfcChange = (e) => {
-        setTfc(e.target.value);
-    };
-
-    const handleQChange = (e) => {
-        setQ(e.target.value);
+    // const handleAfcChange = (e) => {
+    //     setAfc(e.target.value);
+    // };
+    //
+    // const handleTfcChange = (e) => {
+    //     setTfc(e.target.value);
+    // };
+    //
+    // const handleQChange = (e) => {
+    //     setQ(e.target.value);
+    // };
+    const handleInputChange = (index, value) => {
+        const newInputs = [...inputs];
+        newInputs[index].value = value;
+        setInputs(newInputs);
     };
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <label>
-                    AFC:
-                    <input type="number" value={afc} onChange={handleAfcChange} />
-                </label>
-                <br />
-                <label>
-                    TFC:
-                    <input type="number" value={tfc} onChange={handleTfcChange} />
-                </label>
-                <br />
-                <label>
-                    Q:
-                    <input type="number" value={q} onChange={handleQChange} />
-                </label>
-                <br />
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {inputs.map((input, index) => (
+                    <div key={index}>
+                        <label>
+                            {input.name.toUpperCase()}:
+                            <input
+                                type="number"
+                                value={input.value}
+                                onChange={(e) => handleInputChange(index, e.target.value)}
+                            />
+                        </label>
+                        <br />
+                    </div>
+                ))}
             </header>
         </div>
     );
