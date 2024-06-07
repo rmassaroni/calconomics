d859d6c6c69978e0bb149146044f627dd4aba135
rjm <robjmass@gmail.com>
2024-06-07 13:03:28 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
         <div className="App">
             <header className="App-header">
                 {inputs.map((input, index) => (
-                    <div key={index}>
+                    <div className="Line" key={index}>
                         <label>
-                            {input.name.toUpperCase()}:
-                            <input
+                            <p2 style={{ display: "block" }}>{input.name.toUpperCase()}: </p2>
+                            <input style={{ border: "1px solid #ccc", borderRadius: "20px", padding: "5px", marginBottom: "10px" }}
                                 type="number"
                                 value={input.value}
                                 onChange={(e) => handleInputChange(index, e.target.value)}
