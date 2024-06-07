011d7ed0d9efabcf8a84f9ed03cc44c5959a65a2
rjm <robjmass@gmail.com>
2024-06-07 13:48:21 -0400

    show changed to solved

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 
 function App() {
     const [inputs, setInputs] = useState([
-        { name: 'afc', value: 0, show: true },
-        { name: 'tfc', value: 0, show: true },
-        { name: 'q', value: 0, show: true }
+        { name: 'afc', value: 0, solved: false },
+        { name: 'tfc', value: 0, solved: false },
+        { name: 'q', value: 0, solved: false }
     ]);
     const handleInputChange = (index, value) => {
         const newInputs = [...inputs];
@@ -17,30 +17,19 @@ function App() {
             const qValue = parseFloat(newInputs.find(input => input.name === 'q').value);
 
             if (!isNaN(tfcValue) && !isNaN(qValue) && qValue !== 0) {
-                newInputs.find(input => input.name === 'afc').show = false;
+                newInputs.find(input => input.name === 'afc').solved = true;
             } else {
-                newInputs.find(input => input.name === 'afc').show = true;
+                newInputs.find(input => input.name === 'afc').solved = false;
             }
             setInputs(newInputs);
         } 
-        // else if (newInputs[index].name === 'afc' && newInputs[index].show) {
-        //     console.log('true')
-        //     const originalAfcIndex = newInputs.findIndex(input => input.name === 'afc');
-        //     newInputs[originalAfcIndex].show = false;
-        //     setInputs(newInputs);
-        // }
-        if (inputs.find(input => input.name === 'afc').show) {
-            // const originalAfcIndex = newInputs.findIndex(input => input.name === 'afc');
-            // newInputs[originalAfcIndex].show = false;
-            // setInputs(newInputs);
-        }
     };
 
     return (
         <div className="App">
             <header className="App-header">
                 {inputs.map((input, index) => (
-                    input.show ? (
+                    !input.solved ? (
                     <div className="Line" key={index}>
                         <label>
                             <p2 style={{ display: "block" }}>{input.name.toUpperCase()}: </p2>
@@ -58,7 +47,7 @@ function App() {
             <div className="Solutions">
                 {inputs.map((input, index) => (
 
-                    !input.show ? (
+                    input.solved ? (
                     <div className="Line">
                         <label>
                             <p2 style={{ display: "block" }}>AFC</p2>
