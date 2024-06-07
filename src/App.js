8b7a124da53b5717ae6b2fba8d48be6b619cb862
rjm <robjmass@gmail.com>
2024-06-07 15:05:22 -0400

    unnamed commit

--- src/App.js
+++ src/App.js
@@ -24,26 +24,45 @@ function App() {
             setInputs(newInputs);
         } 
     };
+        const handleAddVariable = () => {
+        setInputs(prevInputs => [
+            ...prevInputs,
+            { name: '', value: 0, solved: false }
+        ]);
+    };
+
+    const calculateMinWidth = (value) => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        context.font = '14px Arial'; // Set the font size and type
+        const textWidth = context.measureText(value).width; // Calculate the width of the text
+        return `${textWidth}px`; // Return the minimum width as a string
+    };
 
     return (
         <div className="App">
             <header className="App-header">
                 {inputs.map((input, index) => (
                     !input.solved ? (
-                    <div className="Line" key={index}>
-                        <label>
-                            <p2 style={{ display: "block" }}>{input.name.toUpperCase()}: </p2>
-                            <input style={{ border: "1px solid #ccc", borderRadius: "20px", padding: "5px", marginBottom: "10px" }}
-                                type="number"
-                                value={input.value}
-                                onChange={(e) => handleInputChange(index, e.target.value)}
-                            />
-                        </label>
-                        <br />
-                    </div>
+                        <div className="VarBlock" key={index}>
+                            <label>
+                                <p2 style={{ display: "block" }}>{input.name.toUpperCase()}: </p2>
+                                <input style={{ border: "1px solid #ccc", borderRadius: "20px", padding: "5px", marginBottom: "10px", minWidth: "100px" }}
+                                    type="number"
+                                    value={input.value}
+                                    onChange={(e) => handleInputChange(index, e.target.value)}
+                                />
+                            </label>
+                            <br />
+                        </div>
                     ) : null
                 ))}
-                </header>
+                <div className="VarBlock" style={{ height: "100%" }}>
+                    <label>
+                        <button style={{ display: "inline-block", border: "1px solid #ccc", borderRadius: "20px", padding: "5px", marginBottom: "10px", width: "100%", minWidth: "100px", height: "100%" }} onClick={handleAddVariable}>Add</button>
+                    </label>
+                </div>
+            </header>
             <div className="Solutions">
                 {inputs.map((input, index) => (
 
