11a085b2a4a0a6facf892a397220b5d6ea0ff082
rjm <robjmass@gmail.com>
2024-06-07 12:31:55 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-    // const [afc, setAfc] = useState(0);
-    // const [tfc, setTfc] = useState(0);
-    // const [q, setQ] = useState(0);
     const [inputs, setInputs] = useState([
         { name: 'afc', value: 0 },
         { name: 'tfc', value: 0 },
         { name: 'q', value: 0 }
     ]);
-
-    // const handleAfcChange = (e) => {
-    //     setAfc(e.target.value);
-    // };
-    //
-    // const handleTfcChange = (e) => {
-    //     setTfc(e.target.value);
-    // };
-    //
-    // const handleQChange = (e) => {
-    //     setQ(e.target.value);
-    // };
     const handleInputChange = (index, value) => {
         const newInputs = [...inputs];
         newInputs[index].value = value;
@@ -32,7 +16,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 {inputs.map((input, index) => (
                     <div key={index}>
                         <label>
